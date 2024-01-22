613b30f4647922adde2eede444f66edc69a97ad2
mehmet_yasar <mehmet.yasar0770@gmail.com>
2024-01-22 21:23:27 +0300

    feat : Uygulama Şablonu oluşturuldu

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Tabs App</h1>
+        <div>
+        <nav>
+          <ul>
+              <li> Home</li>
+              <li> Products</li>
+              <li> About </li>
+              <li> Contact</li>
+
+
+          </ul>
+        </nav>
+        </div>
+       
+        </header>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
